4a335d5fa43f089418b0f6dfd5c02ceb99a72bf5
Charlotte Ganske <cganske04@icloud.com>
2024-11-12 11:46:37 -0600

    fixed js

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -7,6 +7,7 @@ let lastName = 'Ganske'
 //     (remember, you already have variables for this, can you use those?)
 //     Console log the value of `fullName`
 let fullName = firstName + ' ' + lastName;
+console.log(fullName)
 // 4 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
 //     Console log the value of `luckyNumber`
 let luckyNumber = 2
@@ -26,9 +27,10 @@ console.log(adventurous)
 // 7 - Create a variable named `food`, and set its value to a string of your favorite food
 //     Console log the value of `food`
 let food = 'pasta'
+console.log(food)
 // 8 - Create a variable called `pets` and set it to the value of the number of pets you have
 //     Console log the value of `pets`
-let pets = 3
+let pets = 1
 console.log(pets)
 // 9 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
 let friendsPets = 2
